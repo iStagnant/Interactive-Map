bd2fce6d736cd200fe058e6959430e9c52cd843c
iStagnant <eliasghafari@disroot.org>
2023-08-23 17:26:07 +0300

    fixed code and added output file

--- map.py
+++ map.py
@@ -69,28 +69,36 @@ for node in system_nodes:
 
         #Loops through system planets and populates the planets list and then using that info it populates description
         if node.planets():
-            for planet in node.planets():
-                planets.append(planet_attributes(planet))
-            for p in planets:
-                #If last planet in system add the government at the end
-                if (list(p)[0] == list(list(planets)[-1])[0]):
-                    name = list(p)[0]
-                    description += f" ```{name.strip(quotes)}```\\n {p[name][0]} {p[name][1]} {p[name][2]}``{government}``\""
-                #If not add an extra \\n to keep the wiki style
-                else:
-                    name = list(p)[0]
-                    description += f" ```{name.strip(quotes)}```\\n {p[name][0]} {p[name][1]} {p[name][2]}\\n"
-            planets = [] #Clear the planets list for the next system to use
+            #Quarg systems can have ringworlds which show up as multiple planets so we check if a Quarg system has more than 3 planets to detect ringworlds, and that's also the case for all Heliarch systems but just in case in the future non ringworld Heliarch systems are added we check
+            if not ((government == "Quarg" or government == "Heliarch") and len(node.planets()) > 3):
+                for planet in node.planets():
+                    planets.append(planet_attributes(planet))
+                for p in planets:
+                    #If last planet in system add the government at the end
+                    if (list(p)[0] == list(list(planets)[-1])[0]):
+                        name = list(p)[0]
+                        description += f" ```{name.strip(quotes)}```\\n  {p[name][0]}  {p[name][1]}  {p[name][2]}``{government}``\""
+                    #If not add an extra \\n to keep the wiki style
+                    else:
+                        name = list(p)[0]
+                        description += f" ```{name.strip(quotes)}```\\n  {p[name][0]}  {p[name][1]}  {p[name][2]}\\n"
+                planets = [] #Clear the planets list for the next system to use
+            #If above conditions are met then we should only take the name of the first planet since the rest are the same
+            else:
+                planet = planet_attributes(node.planets()[0])
+                name = list(planet)[0]
+                description += f" ```{name.strip(quotes)}```\\n  {planet[name][0]}  {planet[name][1]}  {planet[name][2]}``{government}``\""
         else:
-            description += f"  -No Spaceport\\n  -No Shipyard\\n  -No Outfitter\\n''{government}''\","
+            description += f"  -No Spaceport\\n  -No Shipyard\\n  -No Outfitter\\n''{government}''\""
 
+        name = node.name().strip(quotes) #Some systems have "" in their name in the map file so we remove them
         output += f"""{{
             "categoryId": "{ids[government]}",
             "position": [
 
             ],
             "popup": {{
-                "title": {node.name()},
+                "title": "{name}",
                 {description},
                 "link": {{
                     "url": "",
@@ -103,4 +111,5 @@ for node in system_nodes:
         id += 1
         description = "\"description\": \"" #Clear the description for the next system to use
 
-print(output + "]")
+with open("map.txt", "w") as file:
+    file.write(output + "]")
