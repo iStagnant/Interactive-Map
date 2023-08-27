1c9862f9bd0fc66330e3ea21ebb7e55a6322e742
iStagnant <eliasghafari@disroot.org>
2023-08-27 17:13:22 +0300

    Further improving of code

--- map.py
+++ map.py
@@ -28,6 +28,7 @@ ids = {
     "Korath": "10",
     "Kor Efret": "8",
     "Kor Mereti": "9",
+    "Kor Sestor": "25",
     "Pirate": "12",
     "Pug": "13",
     "Quarg": "14",
@@ -42,7 +43,7 @@ ids = {
 remove = ["Terra Incognita", "Over the Rainbow", "World's End"]
 
 #Don't change code under here unless you know what you're doing (I love how this implies I know what I'm doing... I really don't)
-output = "\t"
+output = "    \"markers\": [\n\t"
 description = "\"description\": \""
 quotes = '"'
 id = 1
@@ -52,11 +53,11 @@ def planet_attributes(planet: str):
     for node in planet_nodes:
         if node.node_type == "planet":
             if node.name().strip(quotes) == planet:
-                return {planet : [
+                return [
                     "-Has Spaceport\\n" if node.spaceport() else "-No Spaceport\\n",
                     "-Has Shipyard\\n" if node.shipyards() else "-No Shipyard\\n",
                     "-Has Outfitter\\n" if node.outfitters() else "-No Outfitter\\n",
-                                  ]}
+                                  ]
 
 for node in system_nodes:
     if (node.node_type == "system") and not (node.name().strip(quotes) in remove):
@@ -64,15 +65,10 @@ for node in system_nodes:
         #Getting the government of the system
         if "Quarg" in node.government(): #Because there's Quarg (Gegno), Quarg (Hai) etc...
             government = "Quarg"
-        elif node.government() == '"Kor Sestor"': #Once the player completes the wanderer middle missions they become uninhabited
-            government = "Uninhabited"
         elif "Bunrodea" in node.government(): #In the game file they are split to Bunrodea and Bunrodea (Guard)
             government = "Bunrodea"
         elif "Pirate" in node.government(): #There's a hidden system with government Pirate (Devil-Run Gang)
-            if node.name() == "Men":
-                government = "Independent"
-            else:
-                government = "Pirate"
+            government = "Pirate"
         elif "Pug" in node.government(): #In the game files they are split to Pug and Pug (Wanderer)
             government = "Pug"
         else:
@@ -82,40 +78,53 @@ for node in system_nodes:
         wormholes = []
         for object in node.objects():
             if ("wormhole" in object.sprite()):
-                wormholes.append(object.name())
+                wormholes.append(object.name().strip(quotes))
 
         #Loops through system planets and populates the planets list and then using that info we fill description
-        planets = []
-        encountered_planets = []
+        planets = {}
         for planet in node.planets():
+            planet = planet.strip(quotes)
             #Ringworlds contain multiple planets with the same name so we check if the planet is already in the list
-            if not (planet in wormholes) and not (planet in encountered_planets):
-                encountered_planets.append(planet)
-                planets.append(planet_attributes(planet.strip(quotes)))
+            if not (planet in wormholes) and not (planet in planets):
+                planets[planet] = planet_attributes(planet)
 
-        for p in planets:
-            #If last planet in system add the government at the end
-            if list(p)[0] == list(list(planets)[-1])[0]:
-                name = list(p)[0]
-                description += f" '''{name}'''\\n  {p[name][0]}  {p[name][1]}  {p[name][2]}''{government}''\""
+        #If the system has more than 4 planets description would be too long and cause fandom to error
+        if len(planets) > 4:
+            grouped_planets = {}
+            #We populate grouped_planets which has the attributes of planets as key and the planets that have those attributes as value
+            for name in planets:
+                planet_key = "  ".join(planets[name])
+                if not planet_key in grouped_planets:
+                    grouped_planets[planet_key] = []
 
-            #If not add an extra \\n to keep the wiki style
-            else:
-                name = list(p)[0]
-                description += f" '''{name}'''\\n  {p[name][0]}  {p[name][1]}  {p[name][2]}\\n"
+                grouped_planets[planet_key].append(name)
+
+            #Using the information in grouped_planets we fill description
+            for planet_key, planet_list in grouped_planets.items():
+                description += "".join([f" '''{name}'''," for name in planet_list])
+                description = "\\n  ".join(description.rsplit(",", 1))
+                attributes = planet_key
+
+                if planet_key == list(grouped_planets)[-1]:
+                    description += attributes + f"''{government}''\""
+
+                else:
+                    description += attributes + "\\n"
+
+        else:
+            for name, attributes in planets.items():
+                #If last planet in system add the government at the end
+                if name == list(planets)[-1]:
+                    description += f" '''{name}'''\\n  {attributes[0]}  {attributes[1]}  {attributes[2]}''{government}''\""
+
+                #If not add an extra \\n to keep the wiki style
+                else:
+                    description += f" '''{name}'''\\n  {attributes[0]}  {attributes[1]}  {attributes[2]}\\n"
 
         if (not planets):
             description += f"  -No Spaceport\\n  -No Shipyard\\n  -No Outfitter\\n''{government}''\""
 
-        #These guys have so many planets that it caused fandom to error, and I can't be bothered doing checks for things like this so here's a janky solution
-        if (node.name() == "Arculus"):
-            description = "\"description\": \" '''Seraglio''', '''Eminonu''', '''Babiali''', '''Kumkapi''', '''Xerolophos''', '''Topkapi''', '''Aksaray''', '''Yedikule'''\\n  -No Spaceport\\n  -No Shipyard\\n  -No Outfitter\\n\\n '''Viminal'''\\n  -Has Spaceport\\n  -Has Shipyard\\n  -Has Outfitter\\n''Remnant''\""
-        elif (node.name() == "Pantica"):
-            description = "\"description\": \" '''Capitoline''', '''Quirinal''', '''Servian'''\\n  -No Spaceport\\n  -No Shipyard\\n  -No Outfitter\\n\\n '''Aventine'''\\n  -Has Spaceport\\n  -Has Shipyard\\n  -Has Outfitter\\n\\n '''Esquiline'''\\n  -Has Spaceport\\n  -No Shipyard\\n  -No Outfitter\\n''Remnant''\""
-        elif (node.name() == "Cinxia"):
-            description = "\"description\": \" '''Caelian'''\\n  -Has Spaceport\\n  -Has Shipyard\\n  -Has Outfitter\\n\\n '''Tibernia''', '''Janiculum''', '''Vatican''', '''Palatine''', '''Pincian'''\\n  -No Spaceport\\n  -No Shipyard\\n  -No Outfitter\\n''Remnant''\""
-
-        name = node.name().strip(quotes)
+        planet = node.name().strip(quotes)
         position = node.position()
         x = "{:.3f}".format(base_position[0] + position[0])
         y = "{:.3f}".format(base_position[1] + position[1])
@@ -133,11 +142,11 @@ for node in system_nodes:
                 {y}
             ],
             "popup": {{
-                "title": "{name}",
+                "title": "{planet}",
                 {description},
                 "link": {{
-                    "url": "{name}",
-                    "label": "{name}'s wiki page"
+                    "url": "{planet}",
+                    "label": "{planet}'s wiki page"
                 }}
             }},
             "id": "{id}"
@@ -148,4 +157,4 @@ for node in system_nodes:
         description = "\"description\": \"" #Clear the description for the next system to use
 
 with open("map.txt", "w") as file:
-    file.write(output)
+    file.write("".join(output.rsplit(",\n", 1)) + "\n    ]")
